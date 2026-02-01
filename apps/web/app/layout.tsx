3f0f4f877b43c5209a6c63f032b484f16cc8c27f
abhiram-karanth-core <abhiramkaranth22@gmail.com>
2026-02-02 00:14:48 +0530

    remove WebSocketProvider from app/layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -26,11 +26,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <WebSocketProvider>
+
           <Toaster position="top-right" richColors />
           <Analytics/>
           {children}
-        </WebSocketProvider>
+ 
       </body>
     </html >
   );
